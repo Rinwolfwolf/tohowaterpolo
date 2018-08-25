f8471b8543a9e611a624e1dc4a4bf2c969570b7b
Rinsono <30657479+Rinsono@users.noreply.github.com>
2018-08-25 18:00:24 +0900

    初期化

---
+++ new.js
@@ -0,0 +1,361 @@
+//A5
+$('[name=apple]').change(function () {
+    var val = $('[name=apple]').val();
+    var apple = 'tokutenA5-' + val;
+    localStorage.setItem(apple, '〇');
+});
+$('[name=blue]').change(function () {
+    var val = $('[name=blue]').val();
+    var blue = 'hazushiA5-' + val;
+    localStorage.setItem(blue, '✕');
+});
+//A4　　　　　　　　　　　　　　　　　　　　　　　　　　　
+$('[name=carrot]').change(function () {
+    var val = $('[name=carrot]').val();
+    var carrot = 'tokutenA4-' + val;
+    localStorage.setItem(carrot, '〇');
+});
+$('[name=dream]').change(function () {
+    var val = $('[name=dream]').val();
+    var dream = 'hazushiA4-' + val;
+    localStorage.setItem(dream, '✕');
+});
+//A3
+$('[name=egg]').change(function () {
+    var val = $('[name=egg]').val();
+    var egg = 'tokutenA3-' + val;
+    localStorage.setItem(egg, '〇');
+});
+$('[name=frog]').change(function () {
+    var val = $('[name=frog]').val();
+    var frog = 'hazushiA3-' + val;
+    localStorage.setItem(frog, '✕');
+});
+//A2
+$('[name=ground]').change(function () {
+    var val = $('[name=ground]').val();
+    var ground = 'tokutenA2-' + val;
+    localStorage.setItem(ground, '〇');
+});
+$('[name=horse]').change(function () {
+    var val = $('[name=horse]').val();
+    var horse = 'hazushiA2-' + val;
+    localStorage.setItem(horse, '✕');
+});
+//A1
+$('[name=iron]').change(function () {
+    var val = $('[name=iron]').val();
+    var iron = 'tokutenA1-' + val;
+    localStorage.setItem(iron, '〇');
+});
+$('[name=jam]').change(function () {
+    var val = $('[name=jam]').val();
+    var jam = 'hazushiA1-' + val;
+    localStorage.setItem(jam, '✕');
+});
+//B5
+$('[name=lion]').change(function () {
+    var val = $('[name=lion]').val();
+    var lion = 'tokutenB5-' + val;
+    localStorage.setItem(lion, '〇');
+});
+$('[name=king]').change(function () {
+    var val = $('[name=king]').val();
+    var king = 'hazushiB5-' + val;
+    localStorage.setItem(king, '✕');
+});
+//B4
+$('[name=middle]').change(function () {
+    var val = $('[name=middle]').val();
+    var middle = 'tokutenB4-' + val;
+    localStorage.setItem(middle, '〇');
+});
+$('[name=nest]').change(function () {
+    var val = $('[name=nest]').val();
+    var nest = 'hazushiB4-' + val;
+    localStorage.setItem(nest, '✕');
+});
+//B3
+$('[name=octopus]').change(function () {
+    var val = $('[name=octopus]').val();
+    var octopus = 'tokutenB3-' + val;
+    localStorage.setItem(octopus, '〇');
+});
+$('[name=pig]').change(function () {
+    var val = $('[name=pig]').val();
+    var pig = 'hazushiB3-' + val;
+    localStorage.setItem(pig, '✕');
+});
+//B2
+$('[name=queen]').change(function () {
+    var val = $('[name=queen]').val();
+    var queen = 'tokutenB2-' + val;
+    localStorage.setItem(queen, '〇');
+});
+$('[name=ring]').change(function () {
+    var val = $('[name=ring]').val();
+    var ring = 'hazushiB2-' + val;
+    localStorage.setItem(ring, '✕');
+});
+//B1
+$('[name=still]').change(function () {
+    var val = $('[name=still]').val();
+    var still = 'tokutenB1-' + val;
+    localStorage.setItem(still, '〇');
+});
+$('[name=trap]').change(function () {
+    var val = $('[name=trap]').val();
+    var trap = 'hazushiB1-' + val;
+    localStorage.setItem(trap, '✕');
+});
+//C5
+$('[name=under]').change(function () {
+    var val = $('[name=under]').val();
+    var under = 'tokutenC5-' + val;
+    localStorage.setItem(under, '〇');
+});
+$('[name=violin]').change(function () {
+    var val = $('[name=violin]').val();
+    var violin = 'hazushiC5-' + val;
+    localStorage.setItem(violin, '✕');
+});
+//C4
+$('[name=xylophone]').change(function () {
+    var val = $('[name=xylophone]').val();
+    var xylophone = 'tokutenC4-' + val;
+    localStorage.setItem(xylophone, '〇');
+});
+$('[name=yellow]').change(function () {
+    var val = $('[name=yellow]').val();
+    var yellow = 'hazushiC4-' + val;
+    localStorage.setItem(yellow, '✕');
+});
+//C3
+$('[name=zebra]').change(function () {
+    var val = $('[name=zebra]').val();
+    var zebra = 'tokutenC3-' + val;
+    localStorage.setItem(zebra, '〇');
+});
+$('[name=access]').change(function () {
+    var val = $('[name=access]').val();
+    var access = 'hazushiC3-' + val;
+    localStorage.setItem(access, '✕');
+});
+//C2
+$('[name=bumblebee]').change(function () {
+    var val = $('[name=bumblebee]').val();
+    var bumblebee = 'tokutenC2-' + val;
+    localStorage.setItem(bumblebee, '〇');
+});
+$('[name=counting]').change(function () {
+    var val = $('[name=counting]').val();
+    var counting = 'hazushiC2-' + val;
+    localStorage.setItem(counting, '✕');
+});
+//C1
+$('[name=dangerous]').change(function () {
+    var val = $('[name=dangerous]').val();
+    var dangerous = 'tokutenC1-' + val;
+    localStorage.setItem(dangerous, '〇');
+});
+$('[name=elephant]').change(function () {
+    var val = $('[name=elephant]').val();
+    var elephant = 'hazushiC1-' + val;
+    localStorage.setItem(elephant, '✕');
+});
+//D5
+$('[name=front]').change(function () {
+    var val = $('[name=front]').val();
+    var front = 'tokutenD5-' + val;
+    localStorage.setItem(front, '〇');
+});
+$('[name=giraffe]').change(function () {
+    var val = $('[name=giraffe]').val();
+    var giraffe = 'hazushiD5-' + val;
+    localStorage.setItem(giraffe, '✕');
+});
+//D4
+$('[name=house]').change(function () {
+    var val = $('[name=house]').val();
+    var house = 'tokutenD4-' + val;
+    localStorage.setItem(house, '〇');
+});
+$('[name=inter]').change(function () {
+    var val = $('[name=inter]').val();
+    var inter = 'hazushiD4-' + val;
+    localStorage.setItem(inter, '✕');
+});
+//D3
+$('[name=jungle]').change(function () {
+    var val = $('[name=jungle]').val();
+    var jungle = 'tokutenD3-' + val;
+    localStorage.setItem(jungle, '〇');
+});
+$('[name=knive]').change(function () {
+    var val = $('[name=knive]').val();
+    var knive = 'hazushiD3-' + val;
+    localStorage.setItem(knive, '✕');
+});
+//D2
+$('[name=little]').change(function () {
+    var val = $('[name=little]').val();
+    var little = 'tokutenD2-' + val;
+    localStorage.setItem(little, '〇');
+});
+$('[name=minister]').change(function () {
+    var val = $('[name=minister]').val();
+    var minister = 'hazushiD2-' + val;
+    localStorage.setItem(minister, '✕');
+});
+//D1
+$('[name=note]').change(function () {
+    var val = $('[name=note]').val();
+    var note = 'tokutenD1-' + val;
+    localStorage.setItem(note, '〇');
+});
+$('[name=optimus]').change(function () {
+    var val = $('[name=optimus]').val();
+    var optimus = 'hazushiD1-' + val;
+    localStorage.setItem(optimus, '✕');
+});
+//E5
+$('[name=pick]').change(function () {
+    var val = $('[name=pick]').val();
+    var pick = 'tokutenE5-' + val;
+    localStorage.setItem(pick, '〇');
+});
+$('[name=japan]').change(function () {
+    var val = $('[name=japan]').val();
+    var japan = 'hazushiE5-' + val;
+    localStorage.setItem(japan, '✕');
+});
+//E4
+$('[name=america]').change(function () {
+    var val = $('[name=america]').val();
+    var america = 'tokutenE4-' + val;
+    localStorage.setItem(america, '〇');
+});
+$('[name=france]').change(function () {
+    var val = $('[name=france]').val();
+    var france = 'hazushiE4-' + val;
+    localStorage.setItem(france, '✕');
+});
+//E3
+$('[name=germany]').change(function () {
+    var val = $('[name=germany]').val();
+    var germany = 'tokutenE3-' + val;
+    localStorage.setItem(germany, '〇');
+});
+$('[name=africa]').change(function () {
+    var val = $('[name=africa]').val();
+    var africa = 'hazushiE3-' + val;
+    localStorage.setItem(africa, '✕');
+});
+//E2
+$('[name=india]').change(function () {
+    var val = $('[name=india]').val();
+    var india = 'tokutenE2-' + val;
+    localStorage.setItem(india, '〇');
+});
+$('[name=rossia]').change(function () {
+    var val = $('[name=rossia]').val();
+    var rossia = 'hazushiE2-' + val;
+    localStorage.setItem(rossia, '✕');
+});
+//E1
+$('[name=koria]').change(function () {
+    var val = $('[name=coria]').val();
+    var coria = 'tokutenE1-' + val;
+    localStorage.setItem(coria, '〇');
+});
+$('[name=china]').change(function () {
+    var val = $('[name=china]').val();
+    var china = 'hazushiE4-' + val;
+    localStorage.setItem(china, '✕');
+});
+//F5
+$('[name=ingland]').change(function () {
+    var val = $('[name=ingland]').val();
+    var ingland = 'tokutenF5-' + val;
+    localStorage.setItem(ingland, '〇');
+});
+$('[name=spain]').change(function () {
+    var val = $('[name=spain]').val();
+    var spain = 'hazushiF5-' + val;
+    localStorage.setItem(spain, '✕');
+});
+//F4
+$('[name=tokyo]').change(function () {
+    var val = $('[name=tokyo]').val();
+    var tokyo = 'tokutenF4-' + val;
+    localStorage.setItem(tokyo, '〇');
+});
+$('[name=osaka]').change(function () {
+    var val = $('[name=osaka]').val();
+    var osaka = 'hazushiF4-' + val;
+    localStorage.setItem(osaka, '✕');
+});
+//F3
+$('[name=nagoya]').change(function () {
+    var val = $('[name=nagoya]').val();
+    var nagoya = 'tokutenF3-' + val;
+    localStorage.setItem(nagoya, '〇');
+});
+$('[name=niseko]').change(function () {
+    var val = $('[name=niseko]').val();
+    var niseko = 'hazushiF4-' + val;
+    localStorage.setItem(niseko, '✕');
+});
+//F2
+$('[name=nigata]').change(function () {
+    var val = $('[name=nigata]').val();
+    var nigata = 'tokutenF2-' + val;
+    localStorage.setItem(nigata, '〇');
+});
+$('[name=yokohama]').change(function () {
+    var val = $('[name=yokohama]').val();
+    var yokohama = 'hazushiF2-' + val;
+    localStorage.setItem(yokohama, '✕');
+});
+//F1
+$('[name=kunitachi]').change(function () {
+    var val = $('[name=kunitachi]').val();
+    var kunitachi = 'tokutenF1-' + val;
+    localStorage.setItem(kunitachi, '〇');
+});
+$('[name=obihiro]').change(function () {
+    var val = $('[name=obihiro]').val();
+    var obihiro = 'hazushiF1-' + val;
+    localStorage.setItem(obihiro, '✕');
+});
+
+
+window.onload = function () {
+    document.getElementById("goalin").onclick = function () {
+        countUpA();
+    };
+    document.getElementById("nogoal").onclick = function () {
+        countUpB();
+    };
+}
+var $count = 0;
+function countUpA() {
+    document.getElementById("gooalin").innerHTML = ++$count;
+}
+var $count1 = 0;
+function countUpB() {
+    document.getElementById("nogooal").innerHTML = ++$count1;
+}
+
+
+var shot = document.getElementById('shot');
+shot.onclick = function () {
+    var goalin1 = document.getElementById('goalin1').value;
+    var goalin1Number = parseInt(goalin1, 10);
+    var nogoal1 = document.getElementById('nogoal1').value;
+    var nogoal1Number = parseInt(nogoal1, 10);
+    var ket = document.getElementById('ket');
+    var answer = goalin1Number / (goalin1Number + nogoal1Number) * 100;
+    ket.value = answer;
+}
+
