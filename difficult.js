661796c5bae2fece1bf0dad96668d01be0b708dd
Rinsono <30657479+Rinsono@users.noreply.github.com>
2018-11-02 20:50:08 +0900

    debuggerで検証

--- difficult.js
+++ difficult.js
@@ -1,7 +1,7 @@
 var ready = document.getElementById('ready');
 ready.onclick = function () {
     var clear = document.getElementById('ready');
-
+    debugger;
     if (clear = 777675) {
         location.href = "home.html";
     } else {
